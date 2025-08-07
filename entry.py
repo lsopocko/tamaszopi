8c22129ff9035ac29f9357dcefee85566cc707aa
lukasz.sopocko <lukasz.sopocko@gmail.com>
2025-08-07 16:21:48 +0200

    chore: initial

---
+++ entry.py
@@ -0,0 +1,49 @@
+"""SSD1351 demo (boundaries)."""
+from time import sleep
+from ssd1351 import Display, color565
+from machine import Pin, SPI
+
+RED = const(0XF800)  # (255, 0, 0)
+GREEN = const(0X07E0)  # (0, 255, 0)
+WHITE = const(0XFFF)  # (255, 255, 255)
+
+if not Pin(0, Pin.IN, Pin.PULL_UP).value():
+    raise SystemExit  # skip main.py
+
+def test():
+    
+    do_connect()
+    
+    """Test code."""
+    # Baud rate of 14500000 seems about the max 
+    spi = SPI(2, baudrate=14500000, sck=Pin(7), mosi=Pin(9))
+    print('spi started')
+    display = Display(spi, dc=Pin(2), cs=Pin(1), rst=Pin(3))
+    print('display started')
+
+    w = display.width
+    h = display.height
+
+    display.clear()
+
+    display.draw_rectangle(0, 0, w, h, RED)
+    display.draw_pixel(0, 0, WHITE)
+    display.draw_pixel(0, h - 1, WHITE)
+    display.draw_pixel(w - 1, 0, WHITE)
+    display.draw_pixel(w - 1, h - 1, WHITE)
+    sleep(5)
+
+    display.fill_rectangle(0, 0, w, h, GREEN)
+    sleep(5)
+
+    display.draw_rectangle(0, 0, w, h, RED)
+    display.draw_pixel(0, 0, WHITE)
+    display.draw_pixel(0, h - 1, WHITE)
+    display.draw_pixel(w - 1, 0, WHITE)
+    display.draw_pixel(w - 1, h - 1, WHITE)
+
+    sleep(10)
+    display.cleanup()
+
+
+test()
