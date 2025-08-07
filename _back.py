647197c8954966d322ef648bbbdcd0b04bc343d7
lukasz.sopocko <lukasz.sopocko@gmail.com>
2025-08-07 20:30:50 +0200

    chore: better clear

--- _back.py
+++ _back.py
@@ -1,3 +1,4 @@
+"""SSD1351 demo (boundaries)."""
 from time import sleep
 from ssd1351 import Display, color565
 from machine import Pin, SPI
@@ -10,15 +11,14 @@ menu_items = ["Feed", "Play", "Sleep", "Poop", "Settings"]
 current_index = 0
 selected = False
 
-spi = SPI(1, baudrate=14500000, sck=Pin(7), mosi=Pin(9))
+spi = SPI(2, baudrate=14500000, sck=Pin(7), mosi=Pin(9))
 display = Display(spi, dc=Pin(2), cs=Pin(1), rst=Pin(3))
-display.clear()
 
 if not Pin(0, Pin.IN, Pin.PULL_UP).value():
     raise SystemExit  # skip main.py
 
 def draw_menu():
-    
+    display.clear()  # clear screen
     for i, item in enumerate(menu_items):
         prefix = ">" if i == current_index else " "
         display.draw_text8x8(0, i * 12, f"{prefix} {item}", color565(255, 255, 255))  # white text
@@ -42,17 +42,15 @@ def handle_input():
         sleep(0.5)
 
 def draw_selected_action():
-    display.fill_rectangle(0, 0, 127, 127, color565(0, 0, 0))
+    display.clear()
     display.draw_text8x8(0, 0, f"Selected:", color565(255, 255, 255))
     display.draw_text8x8(0, 12, menu_items[current_index], color565(255, 255, 255))
 
 def main_loop():
-    try:
-        draw_menu()
-        while True:
-            handle_input()
-    except KeyboardInterrupt:
-        print("💡 Exiting cleanly.")
-        
-main_loop()
+    
+    
+    draw_menu()
+    while True:
+        handle_input()
 
+main_loop()
